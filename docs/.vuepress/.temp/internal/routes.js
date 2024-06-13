b49b76a538d3e2ac1608317a5788020edf42750e
2016Messi <wy99599@163.com>
2024-06-13 20:35:27 +0800

    init file

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,7 +1,7 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"程序员考公指南"} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"互联网人考公指南"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
   ["/xxx.html", { loader: () => import(/* webpackChunkName: "xxx.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/xxx.html.js"), meta: {"title":"22"} }],
 ]);
