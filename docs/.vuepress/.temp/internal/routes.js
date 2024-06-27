a4abfe342b083de72d43495121f284fd999195ea
2016Messi <wy99599@163.com>
2024-06-27 19:48:10 +0800

    feat 最新

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -2,6 +2,5 @@ export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"互联网人考公指南"} }],
-  ["/xxx.html", { loader: () => import(/* webpackChunkName: "xxx.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/xxx.html.js"), meta: {"title":"22"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/wangyue/data/new-land/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
